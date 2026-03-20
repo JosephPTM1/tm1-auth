86f113e692abbc1d57d8f59c0a34a8178cf6b5e8
JosephPTM1 <josephpooley26@gmail.com>
2026-03-20 12:05:23 +0000

    Create __init__.py

---
+++ tm1-auth/__init__.py
@@ -0,0 +1,5 @@
+from .auth import get_cam_passport
+from .exceptions import AuthenticationError, PassportTimeoutError
+ 
+__version__ = "0.1.0"
+__all__ = ["get_cam_passport", "AuthenticationError", "PassportTimeoutError"]
